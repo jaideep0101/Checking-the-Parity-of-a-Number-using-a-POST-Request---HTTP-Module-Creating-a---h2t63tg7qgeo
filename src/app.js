c5c4a87432f428194d6e61e81bfd0807569206c8
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-04-22 18:53:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    const chunks = [];
-
-    req.on('data', chunk => {
-      const buf = Buffer.from(chunk);
-      const str = buf.toString();
-      chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value = obj.num1;
-    
-     // Write the code here to check if the number is odd or even
-
-   });
+  if (req.method === "POST") {
+    let json = null;
+    let chunkArray = [];
+    req.on("data", (chunk) => {
+      chunkArray.push(Buffer.from(chunk));
+      json = JSON.parse(chunk);
+    });
+    req.on("end", () => {
+      const number = json.num1;
+      const result = number % 2 === 0 ? "even" : "odd";
+      if (result === "even") {
+        res.statusCode = 200;
+      } else {
+        res.statusCode = 404;
+      }
+      res.setHeader("Content-Type", "text/plain");
+      res.end(`The number ${number} is ${result}`);
+    });
   }
-
-  
 });
-
 
 module.exports = server;
